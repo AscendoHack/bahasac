2c8ec8aa30bff2e1ea3174f55aeb434db38d1b5b
AscendoHack <52598832+AscendoHack@users.noreply.github.com>
2019-07-13 14:50:25 +0700

    Add files via upload

---
+++ c++.py
@@ -0,0 +1,15 @@
+import requests
+import mechanize
+import os
+import time
+os.system('clear')
+
+print "\033[1;32m--------------------------------"
+print "- author   : Entup             -"
+print "- youtube  : AscendoID Tutorial-"
+print "- instagram: Ascendo ID        -"
+print "--------------------------------"
+
+print "Mohon Tunggu...."
+time.sleep(3)
+os.system('xdg-open https://mytreep.icu/7CvpD')
